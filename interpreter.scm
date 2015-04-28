2c416ac590b5f6e4be45dd6c7ccf3d4565fa658f
Peter Downs <peterldowns@gmail.com>
2015-04-28 19:41:44 -0400

    Finish updating interpreter example.

--- interpreter.scm
+++ interpreter.scm
@@ -1,73 +1,81 @@
 ; Functions for expressing the poem as a nested list of constraints.
-(define (poem . args)
-  (cons 'poem args))
-(define (poem? cell)
-  (and (pair? cell)
-       (eq? 'poem (car cell))))
-(define (poem-contents poem)
-  (cdr poem))
+(define (tagged-list tag)
+  (lambda elements (cons tag elements)))
+(define (tagged-list? tag)
+  (lambda (thing)
+    (and (list? thing)
+         (symbol? (car thing))
+         (eq? tag (car thing)))))
 
-(define (line . args)
-  (cons 'line args))
-(define (line? cell)
-  (and (pair? cell)
-       (eq? 'line (car cell))))
-(define (line-contents line)
-  (if (name-of line)
-    (cddr line)
-    (cdr line)))
+(define poem (tagged-list 'poem))
+(define poem? (tagged-list? 'poem))
+(define line (tagged-list 'line))
+(define line? (tagged-list? 'line))
+(define word (tagged-list 'word))
+(define word? (tagged-list? 'word))
 
-(define (word . args)
-  (cons 'word args))
-(define (word? cell)
-  (and (pair? cell)
-       (eq? 'word (car cell))))
+(define (contents thing)
+  (cond ((poem? thing)
+         (cdr thing))
+        ((or (line? thing) (word? thing))
+         (if (name-of thing)
+           (cddr thing)
+           (cdr thing)))
+        (else (error "Unrecognized type of thing:" thing))))
 
-;
 ; Definition of the interpreter.
 (define (name-of cell)
-  (if (> (length cell) 1)
+  (if (and (pair? cell)
+           (> (length cell) 1))
     (let ((name (cadr cell)))
+      ; Not all cells have names
       (if (symbol? name)
         name
         #f))
     #f))
 
+(define (constraints-of cell)
+  (if (pair? cell)
+    (if (name-of cell)
+      (cddr cell)
+      (cdr cell))
+    (error "Not a cell:" cell)))
+
 (define (make-interpreter vocabulary grader)
   (define lines '())
   (define words '())
   (define (interpreter cell)
-    (cond ((poem? cell) (map interpreter (poem-contents cell)))
-          ((line? cell) (map interpreter (line-contents cell)))
-          ((word? cell) #f)
-          ((string? cell) cell)
-          ((symbol? cell) (cons 'symbol cell))
+    (cond ((or (poem? cell) (line? cell))
+           (map interpreter (contents cell)))
+          ((word? cell) 
+           ;; Something like:
+           ;(grader (fetch-words vocabulary (constraints-of cell))))
+           #f)
+          ((string? cell)
+           ; String literals are left as is.
+           cell)
+          ((symbol? cell)
+           ; Not sure what symbols should mean yet.
+           (cons 'symbol cell))
           (else
             (list 'unrecognized cell))))
   interpreter)
 
-;
+
 ; Constraint functions, which based on some interpreter state will either
 ; accept or reject a possibility.
 (define (rhymes-with symbol)
   (lambda (state possibility)
+    (pp "rhymes-with possibility:")
+    (pp possiblity)
     #t))
 
 (define (has-tags . tags)
   (lambda (state possibility)
+    (pp "has-tags possibility:")
+    (pp possiblity)
     #t))
 
-(define test-interpreter (make-interpreter '() '()))
-(define test-constraints (poem
-                           (line 'a (word 'a (has-tags 'noun))
-                                    "literal"
-                                    (word (rhymes-with 'a)))
-                           (line 'b "more literals"
-                                    (word (rhymes-with 'a)))
-                           (line (word (has-tags 'adverb))
-                                 (word "we")
-                                 (word (has-tags 'verb 'past-tense)))))
-(define example (test-interpreter test-constraints))
 
 ; Helper for printing poems
 (define (print-poem poem)
@@ -76,9 +84,27 @@
     (let ((indent (make-list (max depth 0) "\t")))
       (for-each display indent))
     (for-each (lambda (item)
-                (if (pair? item)
+                (if (list? item)
                   (loop item (+ depth 1))
                   (begin (display item)
                          (display " "))))
               poem)
     (newline)))
+
+
+; Example
+(define test-interpreter (make-interpreter 'vocabulary 'grader))
+(define test-word (word 'a (has-tags 'noun) (rhymes-with 'b)))
+(define test-line (line 'b "more-literals" test-word (word (rhymes-with 'a))))
+(define test-constraints (poem
+                           (line 'a (word 'a (has-tags 'noun))
+                                 "literal"
+                                 (word (rhymes-with 'a)))
+                           test-line
+                           (line (word (has-tags 'adverb))
+                                 "we"
+                                 (word (has-tags 'verb 'past-tense)))))
+(define test-poem (test-interpreter test-constraints))
+
+(newline)(newline)
+(print-poem test-poem)
