4dc1d57c09f72ff0c35feb87a402a959488f4bf4
Peter Downs <peterldowns@gmail.com>
2015-04-28 19:41:33 -0400

    Fix indentation.

--- martin-draft.scm
+++ martin-draft.scm
@@ -1,9 +1,9 @@
 
 ;; (poem)
-(define (tagged-list? lst tag)
-  (and (list? lst)
-       (symbol? (car lst))
-       (eq? tag (car lst))))
+(define (tagged-list? thing tag)
+  (and (list? thing)
+       (symbol? (car thing))
+       (eq? tag (car thing))))
 
 (define (poem? poem) (tagged-list? poem 'poem))
 (define (line? line) (tagged-list? line 'line))
@@ -23,36 +23,36 @@
 
 (define (generate-poem poem-template)
   (if (poem? poem-template)
-      (begin
-	(bard:reset-environment)
-	(map generate-line (cdr poem-template)))
-      '()))
+    (begin
+      (bard:reset-environment)
+      (map generate-line (cdr poem-template)))
+    '()))
 
 (define (generate-line line-template)
   (if (line? line-template)
-      (map generate-word (cdr line-template))
-      '()))
+    (map generate-word (cdr line-template))
+    '()))
 
 (define (generate-word word-template)
   (cond ((symbol? word-template) word-template)
-	((word? word-template)
-	 (let ((constraints (generate-constraints (word-constraints word-template))))
-	   (select-word (word-type word-template) constraints)))
-	(else 'NULL))
-      
-(define (select-word type constraints)
-  (let ((word (fetch-word type)))
-    (let (constraint-values
-	  (map (lambda (p) (p word)) constraints))
-      (if (every (lambda (v) (eq? v #t)) constraint-values)
-	  word
-	  (select-word type constraints)))))
+        ((word? word-template)
+         (let ((constraints (generate-constraints (word-constraints word-template))))
+           (select-word (word-type word-template) constraints)))
+        (else 'NULL))
 
-(define (rhyming-pattern name)
-  (lambda (word)
-    (let ((value (bard:get-variable name)))
-      (if value
-	  ((ryhmes-with value) word)
-	  (begin
-	    (bard:set-variable! name word)
-	    #t)))))
+  (define (select-word type constraints)
+    (let ((word (fetch-word type)))
+      (let (constraint-values
+             (map (lambda (p) (p word)) constraints))
+        (if (every (lambda (v) (eq? v #t)) constraint-values)
+          word
+          (select-word type constraints)))))
+
+  (define (rhyming-pattern name)
+    (lambda (word)
+      (let ((value (bard:get-variable name)))
+        (if value
+          ((ryhmes-with value) word)
+          (begin
+            (bard:set-variable! name word)
+            #t)))))
