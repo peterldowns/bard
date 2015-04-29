7bc8c228d8fd418ce13d64e03c75296c81e05611
Peter Downs <peterldowns@gmail.com>
2015-04-29 19:18:51 -0400

    Implement dictionary loading.
    This fixes the definition of a 'word' record, and adds a helper method
    for loading dictionary definitions into a hash table of (word-symbol ->
    word-record).

--- word.scm
+++ word.scm
@@ -12,45 +12,28 @@
   (synonyms word-synonyms)
   (antonyms word-antonyms))
 
-(define (load-file file-name)
+(define (load-file-contents file-name)
   (define (read-file file contents)
     (let ((next-line (read file)))
       (if (eof-object? next-line)
         contents
-        (begin
-          (read-file file
-                     (cons next-line contents))))))
-  (let ((f (open-input-file file-name)))
-    (read-file f '())))
+        (read-file file (cons next-line contents)))))
+  (call-with-input-file
+    file-name
+    (lambda (f) (read-file f '()))))
 
 (define (load-words file-name)
   (let ((table (make-strong-eq-hash-table))
-        (file-contents (load-file file-name)))
+        (file-contents (load-file-contents file-name)))
     (for-each (lambda (line)
                 (if (= 5 (length line))
                   (let ((word (apply make-word line)))
                     (hash-table/put! table (word-sym word) word))
-                  'ignore-broken-content))
+                  ; @Dang: some definitions are broken. Please fix?
+                  (begin
+                    (display "Ignoring definition: ")
+                    (display line)
+                    (newline)
+                    'ignore-broken-content)))
               file-contents)
     table))
-
-
-
-;;Sample Usage
-;(define x (make-word "sorrow" 2
-;		     (list "sad" "melancholy")
-;		     (list  "happy" "gay")))
-;(word? x)
-;;#t
-;
-;(word-spelling x)
-;;"sorrow"
-;
-;(word-syllables x)
-;;2
-;
-;(word-synonyms x)
-;;("sad" "melancholy")
-;
-;(word-antonyms x)
-;;("happy" "gay")
