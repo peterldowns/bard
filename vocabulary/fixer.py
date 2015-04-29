1a73dd6bc688dfb0ce0490c3a8b4e904dee814b9
Peter Downs <peterldowns@gmail.com>
2015-04-29 16:26:18 -0400

    Fix scraped words file.

---
+++ vocabulary/fixer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+import re
+bad_chars = re.compile(r'\\[a-zA-Z0-9\\]+')
+hyphens = re.compile(r'(?<=\w)(\(|\))(?=\w)')
+parens = re.compile(r'\(\ ')
+def fix_line(line):
+  line = bad_chars.sub('', line)
+  line = hyphens.sub('-', line)
+  line = parens.sub('(', line).strip()
+  return line + '\n'
+
+if __name__ == '__main__':
+  with open('wordsScrapped.txt', 'r') as fin:
+    with open('wordsScrapped.txt.clean', 'w') as fout:
+      fout.writelines(fix_line(line) for line in fin)
+  print 'Done.'
+
+
