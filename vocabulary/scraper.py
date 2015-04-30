17a6873bf4fa4041f8c798e0988862a74435831c
Peter Downs <peterldowns@gmail.com>
2015-04-30 00:15:59 -0400

    Update scraper to also output JSON.

--- vocabulary/scraper.py
+++ vocabulary/scraper.py
@@ -1,12 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
 from bs4 import BeautifulSoup
+import json
 import re
 import requests
 
 baseURL = "http://dictionary.reference.com/browse/"
+
+def clean_pos(pos_str):
+  return re.sub(r'\s*\(.*\)\s*', '', pos_str).split(' ')
+
 def clean(inputStr):
 	temp = re.sub("[\d.]|;", "", inputStr).strip()
 	return [y for y in [x.strip() for x in temp.replace("See", "").split(",")] if " " not in y]
-
 
 def scrape(word):
 	req = requests.get(baseURL+word)
@@ -16,7 +22,7 @@ def scrape(word):
 		text = soup.select('.me')[0]['data-syllable'].encode('utf8')
 		synonyms, antonyms = [], []
 		syllables = ''.join([i if ord(i) < 128 else ' ' for i in text]).split()
-		
+
 		if soup.select('.luna-data-header span'):
 			pos = soup.select('.luna-data-header span')[0].string.encode('utf8')
 		else:
@@ -29,12 +35,41 @@ def scrape(word):
 	except (IndexError, KeyError):
 		return ""
 
-fout = open('wordsScrapped.txt', 'a')
-fin = open('words.txt','r')
-line = fin.readline()
-fin.close()
-wordlist = line.split()
-for word in wordlist:
-	fout.write(str(scrape(word)) +"\n")
-	print "finished", word
-fout.close()
+if __name__ == '__main__':
+  wordlist = []
+  with open('words.txt', 'r') as fin:
+    wordlist = fin.readline().split()
+
+  rhymes = {}
+  with open('rhymes.json', 'r') as fin:
+    rhymes = json.load(fin)
+
+  num_words = len(wordlist)
+  json_data = {}
+  str_data = []
+  for i, word in enumerate(wordlist):
+    print 'Scraping {}/{} {}'.format(i + 1, num_words, word)
+    scrape_data = scrape(word)
+
+    word, pos, num_syllables, synonyms, antonyms = scrape_data
+    rhyming_words = rhymes.get(word, [])
+    pos = clean_pos(pos)
+    json_data[word] = {
+        'pos': pos,
+        'num_syllables': num_syllables,
+        'synonyms': synonyms,
+        'antonyms': antonyms,
+        'rhymes': rhyming_words,
+      }
+    str_data.append([word, pos, num_syllables, synonyms, antonyms, rhyming_words])
+
+  print 'Writing text data.'
+  with open('scrape.txt', 'w') as fout:
+    for data in str_data:
+      fout.write(str(data) + '\n')
+
+  print 'Writing JSON data.'
+  with open('scrape.json', 'w') as fout:
+    json.dump(json_data, fout, indent=2)
+
+  print 'Done.'
