be7060849a9878c6f7ec0fd1853da2a613a63e6a
Peter Downs <peterldowns@gmail.com>
2015-04-30 00:33:55 -0400

    Add grader functions.

---
+++ grader.scm
@@ -0,0 +1,29 @@
+(define (first-pick possibilities)
+  (if (null? possibilities)
+    #f
+    (car possibilities)))
+
+(define (random-choice possibilities)
+  (if (null? possibilities)
+    #f
+    (let* ((count (length possibilities))
+           (index (random count))
+           (item (list-ref possibilities index)))
+      item)))
+
+(define (most-syllables possibilities)
+  (if (null? possibilities)
+    #f
+    (let loop ((longest #f)
+               (possibilities possibilities))
+      (let ((test (car possibilities))
+            (remaining (cdr possibilities)))
+        (if (null? remaining)
+          longest
+          (loop
+            (if (or (not longest)
+                    (> (word-num-syllables test)
+                       (word-num-syllables longest)))
+              test
+              longest)
+            remaining))))))
