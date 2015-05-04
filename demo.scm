7e9a6a507e029a5e7013400b99f98bc1cb191471
Peter Downs <peterldowns@gmail.com>
2015-05-04 11:00:05 -0400

    Add better demos.

--- demo.scm
+++ demo.scm
@@ -2,29 +2,48 @@
 
 ; Example
 (define load-result (load-words "vocabulary/scrape.scm"))
-(define test-vocabulary (car load-result))
-(define test-pos-table (cdr load-result))
-(define test-interpreter (make-interpreter test-vocabulary dummy-grader))
-(define test-word (match-word 'a (number-syllables 2)
-                                 (rhymes-with "clicker")))
-(define test-line (match-line 'b "more-literals"
-                                 test-word
-                                 (match-word (rhymes-with "corruption"))))
-(define test-constraints (poem
-                           (match-line 'a
-                                       ;'(syllables 10)
-                                       (match-word 'y (has-synonym "learning"))
-                                       (match-word 'x (rhymes-with "investing")))
-                           (match-line test-word)
-                           (match-line '(syllables 6)
-                                       (match-word (any-word)))
-                           (match-line '(syllables 10)
-                                       (match-word (any-word))
-                                       (match-word (any-word)))
-                           (match-line 'a)
-                           (match-line (match-word 'x)
-                                       (match-word 'a))
-                           test-line))
-(define (test)
-  (let ((test-poem (test-interpreter test-constraints)))
-    test-poem))
+(define vocab (car load-result))
+(define pos-table (cdr load-result))
+(define interpreter (make-interpreter vocab random-choice))
+
+(define test-haiku (poem
+                    (match-line 'a
+                                '(syllables 5)
+                                (match-word (any-word))
+                                (match-word (any-word)))
+                    (match-line 'b
+                                '(syllables 7)
+                                (match-word (rhymes-with "allay"))
+                                (match-word (any-word)))
+                    (match-line 'c
+                                '(syllables 5)
+                                (match-word (any-word))
+                                (match-word (any-word)))))
+#|
+(print-poem (interpreter test-haiku))
+; sauciness bayoneted
+; crochet honoraria
+; suffused overtook
+|#
+
+(define test-poem (poem
+                    (match-line 'a
+                                ;'(syllables 10)
+                                (match-word 'y (has-synonym "learning"))
+                                (match-word 'x (rhymes-with "investing")))
+                    (match-line '(syllables 6)
+                                (match-word (any-word)))
+                    (match-line '(syllables 10)
+                                (match-word (any-word))
+                                (match-word (any-word)))
+                    (match-line 'a)
+                    (match-line (match-word 'x)
+                                (match-word 'a))))
+#|
+(print-poem (interpreter test-poem))
+; unlearning vesting
+; mariposas
+; dirigibles mariposas
+; unlearning vesting
+; vesting enunciates
+|#
