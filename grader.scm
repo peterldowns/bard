bd53e0539c16ab40d18dfc03090528a0e8d0ee72
Peter Downs <peterldowns@gmail.com>
2015-05-03 22:28:15 -0400

    Remove broken graders.

--- grader.scm
+++ grader.scm
@@ -1,34 +1,3 @@
-(define (universal-grader possibilities) possibilities)
-
 ; TODO(peter): redefine graders to return ordered lists of possibilities, not
-; single words. The following graders are broken:
-
-(define (first-pick possibilities)
-  (if (null? possibilities)
-    #f
-    (car possibilities)))
-
-(define (random-choice possibilities)
-  (if (null? possibilities)
-    #f
-    (let* ((count (length possibilities))
-           (index (random count))
-           (item (list-ref possibilities index)))
-      item)))
-
-(define (most-syllables possibilities)
-  (if (null? possibilities)
-    #f
-    (let loop ((longest #f)
-               (possibilities possibilities))
-      (let ((test (car possibilities))
-            (remaining (cdr possibilities)))
-        (if (null? remaining)
-          longest
-          (loop
-            (if (or (not longest)
-                    (> (word-num-syllables test)
-                       (word-num-syllables longest)))
-              test
-              longest)
-            remaining))))))
+; single words.
+(define (universal-grader possibilities) possibilities)
